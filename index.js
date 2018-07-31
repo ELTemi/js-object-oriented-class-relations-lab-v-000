bdc95ff48887f5f8981ed388354e1eb0dedbef46
Ruth <github email address>
2018-07-30 20:16:00 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,79 @@
+let driverId = 0
+const store = {drivers: [], passengers: [], trips: []}
+
+class Driver {
+  constructor(name) {
+    this.id = ++driverId
+    this.name = name
+
+    store.drivers.push(this)
+  }
+
+
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.driverId === this.id
+
+    })
+  }
+
+  passengers() {
+    return store.passengers.filter(passenger => {
+      return passenger.trips()
+    })
+  }
+
+}
+
+let passengerId = 0
+
+class Passenger {
+  constructor(name) {
+    this.id = ++passengerId
+    this.name = name
+
+    store.passengers.push(this)
+  }
+
+
+
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.passengerId === this.id
+    })
+  }
+
+  drivers() {
+    return store.drivers.filter(driver => {
+      return driver.trips()
+    })
+  }
+
+}
+
+let tripId = 0
+
+class Trip{
+  constructor(driver, passenger) {
+    this.id = ++tripId
+    this.driverId = driver.id
+    this.passengerId = passenger.id
+
+    store.trips.push(this)
+  }
+
+
+  passenger(){
+     store.passengers.find(function(passenger) {
+      return passenger.id == this.passengerId
+    })
+  }
+
+  driver(){
+    return store.drivers.find(function(driver){
+      return driver.id == this.driverId
+    })
+  }
+
+
+}
